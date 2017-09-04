8cb4a666e0eb859a79290788fb3392fa07c0d635
Zachary Calderone <zacharycalderone@Zacharys-MacBook-Pro.local>
2017-09-04 15:49:22 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hi! My name is Zac");
+        System.out.println("I like to game, my dad introduced me to gaming from a very young age");
+        System.out.println("I've been programming since 2012, which makes 5 years.");
+        System.out.println("I first learned the very basics of C++");
+        System.out.println("Then I took an AP course for Java and did very well and have mostly used Java since then, plus some swift");
+        System.out.println("Now I'm here to learn how to make apps so I can be a freelance app designer");
+    }
+}
